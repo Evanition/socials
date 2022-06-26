4bbec469ed64c671c0ac61ec4eccaad438a94537
Evanition <95145810+Evanition@users.noreply.github.com>
2022-06-26 14:19:24 -0400

    Add files via upload

---
+++ bees.js
@@ -0,0 +1,24 @@
+function Youtube() {
+    window.open(
+      "https://www.youtube.com/channel/UCGY7N0dhiWC9D88986M3LXg", "_blank");
+}
+function Linkdn() {
+  window.open(
+    "https://www.linkedin.com/in/dewayne-barnes-b081b0225/", "_blank");
+}
+function Reddit() {
+  window.open(
+    "https://www.reddit.com/user/IndustrialPlumbing", "_blank");
+}
+function Discord() {
+  window.open(
+    "https://discord.gg/uemdzrv2", "_blank");
+}
+function Twitter() {
+  window.open(
+    "https://twitter.com/ConnorEatSports", "_blank");
+}
+function Github() {
+  window.open(
+    "https://github.com/Evanition", "_blank");
+}
